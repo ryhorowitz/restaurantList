b92af0e94b138e9e1f26131326b71b49becdb5e9
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-01-11 23:36:16 -0500

    EOD need to next autopopulate restaurants with Googs-Maps-Places

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-// require('dotenv').config();
 const express = require('express');
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + './../public/distro'));
+app.use(express.static(path.resolve(__dirname + './../public/distro'))
 
 app.listen(PORT, '127.0.0.1', (err) => {
   if (err) console.error('ERROR IN SERVER LISTEN', err);
